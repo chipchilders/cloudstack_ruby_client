a276c1c535abe09aef14c63451746da4a47cac5e
Isaac Chiang <isaacchiang@gmail.com>
2013-08-23 11:57:24 +0800

    Add login & logout api commands

--- lib/cloudstack_ruby_client/client.rb
+++ lib/cloudstack_ruby_client/client.rb
@@ -72,4 +72,85 @@ class CloudstackRubyClient::Client < CloudstackRubyClient::BaseClient
   ## Firewall command injection
   include CloudstackRubyClient::Firewall::Firewall
 
+  def login(params = {})
+    params['response'] = 'json'
+    params['command']  = 'login'
+    params_arr = []
+    params.each do |elem|
+      params_arr << elem[0].to_s + '=' + CGI.escape(elem[1].to_s)\
+                                            .gsub('+', '%20').gsub(' ','%20')
+    end
+
+    data = params_arr.sort.join '&'
+
+    url = "#{@api_url}?#{data}"
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    #http.use_ssl = @use_ssl
+    #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    resp_title = "loginresponse"; 
+
+    response = http.request(request)
+
+    if !response.is_a?(Net::HTTPOK);
+      if response.code =~ /(431|530)/ &&
+         (JSON.parse(response.body)[resp_title]['cserrorcode'] == 9999 ||
+          JSON.parse(response.body)[resp_title]['cserrorcode'] == 4350);
+
+         raise ArgumentError, JSON.parse(response.body)\
+                                                [resp_title]['errortext'];
+
+      end;
+
+      raise RuntimeError, JSON.parse(response.body)\
+                                           ['errorresponse']['errortext'];
+    end;
+                                                           
+    json = JSON.parse(response.body);
+
+    json[resp_title]
+  end
+
+  def logout(params = {})
+    params['response'] = 'json'
+    params['command']  = 'logout'
+    params_arr = []
+    params.each do |elem|
+      params_arr << elem[0].to_s + '=' + CGI.escape(elem[1].to_s)\
+                                            .gsub('+', '%20').gsub(' ','%20')
+    end
+
+    data = params_arr.sort.join '&'
+
+    url = "#{@api_url}?#{data}"
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    #http.use_ssl = @use_ssl
+    #http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Get.new(uri.request_uri)
+
+    resp_title = "logoutresponse"
+
+    response = http.request(request)
+
+    if !response.is_a?(Net::HTTPOK);
+      if response.code =~ /(431|530)/ &&
+         (JSON.parse(response.body)[resp_title]['cserrorcode'] == 9999 ||
+          JSON.parse(response.body)[resp_title]['cserrorcode'] == 4350);
+
+         raise ArgumentError, JSON.parse(response.body)\
+                                                [resp_title]['errortext'];
+
+      end;
+
+      raise RuntimeError, JSON.parse(response.body)\
+                                           ['errorresponse']['errortext'];
+    end;
+
+    json = JSON.parse(response.body);
+
+    json[resp_title]
+  end
 end
